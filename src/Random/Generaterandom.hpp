2f11f26a38b60e9aa05d5a5d8bcfca3d604eaa46
cleyvraz <32456387+cleyvraz@users.noreply.github.com>
2017-11-12 18:50:19 +0100

    Delete Generaterandom.hpp

--- src/Random/Generaterandom.hpp
+++
@@ -1,8 +0,0 @@
-#ifndef GENERATERANDOM_HPP
-#define GENERATERANDOM_HPP
-
-#include <random>
-
-unsigned int generaterandom(unsigned int,unsigned int); //!<generate a random number distribution (uniform) inbetween 2 unsigned integers (used to make random connections)
-
-#endif 
